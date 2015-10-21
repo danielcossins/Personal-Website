2bbc9877e9bebf2fe31252197b4b5c630c1d0e5d
danielcossins11 <daniel.cossins@yahoo.com>
2015-10-21 10:38:58 -0500

    button click working with vanillajs

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,15 @@
-var $circles = $(".circle");
-console.log($circles);
-$circles.on("click", function(){
-  console.log("clicked");
+about.addEventListener("click", function(){
+  console.log("about");
+});
+
+contact.addEventListener("click", function(){
+  console.log("contact");
+});
+
+projects.addEventListener("click", function(){
+  console.log("projects");
+});
+
+something.addEventListener("click", function(){
+  console.log("something");
 });
