f99cca270c3e9ed49373b5a7edf24e3151c23257
danielcossins11 <daniel.cossins@yahoo.com>
2015-10-28 22:44:30 -0500

    sliding hiding and showing based on time

--- scripts/main.js
+++ scripts/main.js
@@ -8,10 +8,21 @@ about.addEventListener("click", function(){
   console.log("about");
   // main.style.display = "none";
   // mobile.style.display = "none";
-  aboutpgMain.style.display = "";
-  aboutpgMobile.style.display = "";
-  mainPic.style.display = "none";
-  mobilePic.style.display = "none";
+  mainPic.classList.add("animated");
+  mainPic.classList.add("fadeOut");
+  setTimeout(function(){
+    mainPic.style.display = "none";
+    aboutpgMain.style.display = "";
+    aboutpgMobile.style.display = "";
+  }, 1000);
+  // mainPic.classList.add("slideOutLeft");
+  // mainPic.style.display = "none";
+
+
+
+  // mobilePic.classList.add("animated");
+  // mobilePic.classList.add("fadeOut");
+  // mobilePic.style.display = "none";
 });
 
 contact.addEventListener("click", function(){
